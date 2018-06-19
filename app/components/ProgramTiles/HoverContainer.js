c37b7fea20bb21a8974ab3e32d3d51f0188e1cfc
Ethan Dinnen <ethan.dinnen@ocean.org>
2018-06-19 11:29:49 -0700

    Updates to responsive vertical centering for components/ProgramTiles & components/ResearchOverviewTiles HoverContainers

--- app/components/ProgramTiles/HoverContainer.js
+++ app/components/ProgramTiles/HoverContainer.js
@@ -3,19 +3,33 @@ import * as mixins from 'styles/mixins';
 
 const HoverContainer = styled.div`
   z-index: 10;
-  position: relative;
-  top: 65px;
   padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: inherit;
+  h1 {
+    margin: 0 auto;
+    z-index: 10;
+    align-self: flex-start;
+  }
+  div {
+    z-index: 10;
+  }
+  button {
+    align-self: flex-end;
+  }
   ${mixins.bp.sm.max`
-    height: 219px;
     h1 {
       font-size: 26px;
-      margin-bottom: 5px;
-      margin-top: -5px;
     }
     button {
       margin-top: 15px !important;
     }
+  `}
+  ${mixins.bp.xs.max`
+    top: unset;
   `}
 `;
 
--- app/components/ResearchOverviewTiles/HoverContainer.js
+++ app/components/ResearchOverviewTiles/HoverContainer.js
@@ -3,17 +3,27 @@ import * as mixins from 'styles/mixins';
 
 const HoverContainer = styled.div`
   z-index: 10;
-  position: relative;
-  top: 65px;
   padding: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: inherit;
+  h1 {
+    margin: 0 auto;
+    z-index: inherit;
+    align-self: flex-start;
+  }
+  div {
+    z-index: inherit;
+  }
+  button {
+    align-self: flex-end;
+  }
   ${mixins.bp.sm.max`
-    top: 0;
-    height: 219px;
     padding-top: 15px;
     h1 {
       font-size: 26px;
-      margin-bottom: 5px;
-      margin-top: -5px;
     }
     button {
       margin-top: 15px !important;
