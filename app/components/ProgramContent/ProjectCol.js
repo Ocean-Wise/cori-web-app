0edee010a7e240238968cdf273c5934c6bf4764c
Ethan Dinnen <ethan.dinnen@ocean.org>
2018-09-11 15:29:15 -0700

    ProgramContent project list columns padding-left: 0 for all

--- app/components/ProgramContent/ProjectCol.js
+++ app/components/ProgramContent/ProjectCol.js
@@ -2,9 +2,7 @@ import styled from 'styled-components';
 import { Col } from 'react-flexbox-grid';
 
 const ProjectCol = styled(Col)`
-  &:first-of-type {
-    padding-left: 0;
-  }
+  padding-left: 0;
 `;
 
 export default ProjectCol;
